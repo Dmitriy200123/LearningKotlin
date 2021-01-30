97cd68277a66e706dbe0a1d0b0e9dc15eedc9e39
Dima <d.d.demin@urfu.me>
2021-01-30 12:54:20 +0500

    Comparison of numbers

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,11 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val number = scan.nextInt()
+    val result = when {
+        number == 0 -> "Число равно нулю"
+        number > 0 -> "Число положительное"
+        else -> "Число отрицательное"
+    }
+    println(result)
 }
