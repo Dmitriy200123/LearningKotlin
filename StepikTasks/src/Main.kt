183028459cb46e41f7fc5c158eac31aeed683d8a
Dima <d.d.demin@urfu.me>
2021-01-30 11:59:58 +0500

    Operations with numbers

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,8 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val first = scan.nextInt()
+    val second = scan.nextInt()
+    val operation = first / 100 + first % 10 + second / 100 + second % 10
+    println(operation)
 }
