dc0de3cd4ff4369c180a4d8cc7e9135d51f4674f
Dima <d.d.demin@urfu.me>
2021-01-30 12:32:54 +0500

    Operations with strings

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,6 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val line = scan.nextLine()
+    println("${line[0]}F${line.substring(2, line.length-2)}L${line.last()}")
 }
