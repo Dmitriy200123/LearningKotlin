a8ff585d28bfe08951beed7b39e18399a020013b
Dima <d.d.demin@urfu.me>
2021-01-31 11:32:01 +0500

    tasks on the studied topics

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,7 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val values = scan.nextLine().split(' ')
+    val numbers = values.take(values.count() - 1).map { it.toInt() }
+    println(if (values.last().toBoolean()) numbers.min() else numbers.max())
 }
