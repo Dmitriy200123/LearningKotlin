6445062732f3ee116945cb23664a899d5e754239
Dima <d.d.demin@urfu.me>
2021-01-31 10:51:55 +0500

    Operator "when" and count of digits in a number

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,11 @@
 import java.util.*
+import kotlin.math.abs
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    when (abs(scan.nextInt())){
+        in 10 .. 99 -> println("A")
+        in 100 .. 999 -> println("B")
+        else -> println("C")
+    }
 }
