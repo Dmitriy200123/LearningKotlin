746965a59a8e30b957c3ea09d524cf565eed3c0e
Dima <d.d.demin@urfu.me>
2021-01-31 11:13:55 +0500

    The area of a circle

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,7 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val number = scan.nextDouble()
+    val result = Math.round(Math.PI * number * number)
+    println(result)
 }
