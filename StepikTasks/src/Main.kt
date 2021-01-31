b9800623055acb78b9b7dfcbda98530cda7e1c55
Dima <d.d.demin@urfu.me>
2021-01-31 12:41:41 +0500

    operations with strings

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,14 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val values = scan.nextLine().split(' ')
+    val pipeDescription = values[0]
+    val breakdownSymbol = values[1]
+    val result = when{
+        pipeDescription.startsWith(breakdownSymbol) -> 1
+        pipeDescription.endsWith(breakdownSymbol) -> 3
+        pipeDescription.contains(breakdownSymbol) -> 2
+        else -> 4
+    }
+    println(result)
 }
