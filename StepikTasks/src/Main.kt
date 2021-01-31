5ab1f832074d228c0ae1f8101702dfe81d979087
Dima <d.d.demin@urfu.me>
2021-01-31 12:17:37 +0500

    tasks on the studied topics

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,16 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val first = scan.nextInt()
+    val second = scan.nextDouble()
+    val result = when (first){
+        1 -> Math.pow(second, second)
+        2 -> Math.floor(second).toInt()
+        3 -> Math.ceil(second).toInt()
+        4 -> Math.round(second).toInt()
+        5 -> Math.abs(second)
+        6 -> Math.sqrt(second)
+        else -> 2 * second * second
+    }
+    println(result)
 }
