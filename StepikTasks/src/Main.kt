26da2c43f731a6f84d5f56a8f081cf6e6422f770
Dima <d.d.demin@urfu.me>
2021-01-31 11:39:40 +0500

    tasks on the studied topics

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,7 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val values = Array(6){_ -> scan.nextInt()}
+    val tripCost = values[0] * values[1] * values[4] + values[2] * values[4] + 2 * values[3] - values.last()
+    println(tripCost)
 }
