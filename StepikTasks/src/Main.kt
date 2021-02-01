62197e5f470485f0b9a2cc57115d83408a78b9da
Dima <d.d.demin@urfu.me>
2021-02-01 11:05:08 +0500

    Functions

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,12 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+fun main() {
+    val s1 = scan.next()
+    val s2 = scan.next()
+    val b  = scan.nextBoolean()
+    print(check(s1, s2, ignoreCase = b))
 }
+
+fun check(first: String, second: String, ignoreCase: Boolean = false)
+    = first[0].equals(second[0], ignoreCase)
