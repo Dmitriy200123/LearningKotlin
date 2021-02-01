9c1d71377e6a2cee07419f7e424158169e28a707
Dima <d.d.demin@urfu.me>
2021-02-01 12:26:13 +0500

    Arrays and loops

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,17 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    var balance = scan.nextInt()
+    var operations = 0
+    while (scan.hasNextInt()){
+        val currentOperation = scan.nextInt()
+        if (balance + operations + currentOperation < 0) {
+            balance += operations
+            println("Баланс - $balance руб, нужно - ${Math.abs(currentOperation)} руб")
+            return
+        }
+        operations += currentOperation
+    }
+    balance += operations
+    println("Баланс - $balance руб")
 }
