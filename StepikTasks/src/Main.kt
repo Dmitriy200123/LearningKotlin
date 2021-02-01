a379d6a7a273711f92013ae290f690332f1d10df
Dima <d.d.demin@urfu.me>
2021-02-01 11:37:03 +0500

    Arrays and loops

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,7 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val numbers = IntArray(scan.nextInt()){scan.nextInt()}
+    val desiredNumber = scan.nextInt()
+    println(numbers.filter { it == desiredNumber }.sum())
 }
