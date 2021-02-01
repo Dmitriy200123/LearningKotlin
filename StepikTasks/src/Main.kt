3b5404b3c0ce4bf8c1adfbe71ab2c1ead7112f9e
Dima <d.d.demin@urfu.me>
2021-02-01 10:52:26 +0500

    Functions

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,11 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+fun main() {
+    val s = scan.next()
+    print(isCapitalized(s))
+}
+
+fun isCapitalized(s: String): Boolean{
+    return s.first().isUpperCase()
 }
