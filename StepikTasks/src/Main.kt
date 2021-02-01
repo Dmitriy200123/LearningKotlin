af13da4e3488b9b6b2c81f038ff43048a139dfbc
Dima <d.d.demin@urfu.me>
2021-02-01 10:39:04 +0500

    Arrays and loops

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,9 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    val n = scan.nextInt()
+    val words = Array<String>(10){""}
+    for (i in n-1 downTo 0)
+        words[i] = scan.next().reversed()
+    println(words.joinToString(" "))
 }
