20a17d10badba07f7b36cd22bcd8df964164acc0
Dima <d.d.demin@urfu.me>
2021-02-01 11:42:23 +0500

    Arrays and loops

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -2,5 +2,11 @@ import java.util.*
 
 val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    var maxNumber = Int.MIN_VALUE
+    while(scan.hasNextInt()){
+        val currentNumber = scan.nextInt()
+        if (maxNumber < currentNumber)
+            maxNumber = currentNumber
+    }
+    println(maxNumber)
 }
