abeec2d592c181a4b0b76fb1dfeebdf81ccafb27
Dima <d.d.demin@urfu.me>
2021-02-01 10:50:34 +0500

    Arrays and loops

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,12 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+fun main() {
+    val a = scan.nextInt()
+    val b = scan.nextInt()
+    print(calc(a,b))
+}
+
+fun calc(a: Int, b: Int): Int{
+    return 7 * (a * a - b * b * b)  * (a - b)
 }
