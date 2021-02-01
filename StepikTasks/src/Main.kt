5006c082c8d987dfbf67564990326c2b16693613
Dima <d.d.demin@urfu.me>
2021-02-01 10:56:18 +0500

    Functions

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,13 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+fun main() {
+    val c1 = scan.next()[0]
+    val c2 = scan.next()[0]
+    print(isCaseEquals(c1,c2))
+}
+
+fun isCaseEquals(firstSymbol: Char, secondSymbol: Char): Boolean{
+    return firstSymbol.isUpperCase() && secondSymbol.isUpperCase()
+                || firstSymbol.isLowerCase() && secondSymbol.isLowerCase()
 }
