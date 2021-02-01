6d5f158b7b7c766e5eeb33858558d76b11b84813
Dima <d.d.demin@urfu.me>
2021-02-01 11:06:51 +0500

    Functions

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,15 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+fun main() {
+    val s1 = scan.next()
+    val s2 = scan.next()
+
+    if(scan.hasNext()) // Если на вход подается Boolean
+        print(check(s1,s2,scan.nextBoolean()))
+    else
+        print(check(s1,s2))
 }
+
+fun check(first: String, second: String, ignoreCase: Boolean = false)
+        = first[0].equals(second[0], ignoreCase)
