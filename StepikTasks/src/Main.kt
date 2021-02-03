6b057e362c829c1927fdc3ec7bd34709d32162af
Dima <d.d.demin@urfu.me>
2021-02-03 09:56:53 +0500

    OOP

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,32 @@
 import java.util.*
 
 val scan = Scanner(System.`in`)
-fun main(){
-    println(scan.nextInt())
+
+fun makeCatVoice(e: Cat){
+    e.voice()
+}
+open class Cat {
+    open fun voice() {
+        println("Meow meow meow!!!")
+    }
+}
+
+class HomeCat(val name: String): Cat(){
+    override fun voice() {
+        println("Meoooooow! Feed me, $name!")
+    }
+}
+
+fun main() {
+    val usualCat = Cat()        // Создаем обычного кота
+
+    val name = scan.next()      // Считываем имя
+    val homeCat = HomeCat(name) // Передаём в конструктор
+
+    val num = scan.nextInt()
+
+    repeat(num) {
+        makeCatVoice(usualCat)
+        makeCatVoice(homeCat)
+    }
 }
