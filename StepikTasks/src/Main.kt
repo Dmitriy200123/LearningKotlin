bb4abb2484d8eae4aafa39edde9ea9c4aad9899c
Dima <d.d.demin@urfu.me>
2021-02-03 10:18:15 +0500

    Enum and interface

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -4,3 +4,14 @@ val scan = Scanner(System.`in`)
 fun main(){
     println(scan.nextInt())
 }
+
+enum class LogLevel {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+}
+
+fun logMessage(level: LogLevel, message: String) {
+    println("[${level.name}]: $message")
+}
