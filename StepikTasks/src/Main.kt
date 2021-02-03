fe5bdfbccefeee6cff499c50c5367b026c7c031b
Dima <d.d.demin@urfu.me>
2021-02-03 10:24:39 +0500

    Enum and interface

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -1,6 +1,27 @@
 import java.util.*
 
-val scan = Scanner(System.`in`)
 fun main(){
-    println(scan.nextInt())
+    println(Navigator()
+        .getDestination(arrayOf(Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.RIGHT, Direction.UP)))
+}
+
+enum class Direction {
+    UP, RIGHT, DOWN, LEFT
+}
+
+class Navigator {
+    fun getDestination(path: Array<Direction>): String {
+        var coordinateX = 0
+        var coordinateY = 0
+        for (direction in path){
+            when(direction){
+                Direction.UP -> coordinateY++
+                Direction.DOWN -> coordinateY--
+                Direction.RIGHT -> coordinateX++
+                Direction.LEFT -> coordinateX--
+            }
+        }
+
+        return "(x: $coordinateX, y: $coordinateY)"
+    }
 }
