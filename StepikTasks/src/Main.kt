eeb5d3a3344b88afe3528b40425ba836652cd5ed
Dima <d.d.demin@urfu.me>
2021-02-03 10:58:07 +0500

    Nullability

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -4,3 +4,28 @@ val scan = Scanner(System.`in`)
 fun main(){
     println(scan.nextInt())
 }
+
+interface PersonFormatter {
+    fun format(person: Person?): String
+}
+
+class Address (
+    val city: String,
+    val street: String?
+)
+
+class Person (
+    val name: String,
+    val address: Address?
+)
+
+class SimplePersonFormatter: PersonFormatter{
+    override fun format(person: Person?): String {
+        if (person == null)
+            return ""
+        val name = person.name.capitalize()
+        val city = person.address?.city?.capitalize() ?: return name
+        val street = person.address.street?.toUpperCase() ?: return "$name ($city)"
+        return "$name ($city, ул. $street)"
+    }
+}
