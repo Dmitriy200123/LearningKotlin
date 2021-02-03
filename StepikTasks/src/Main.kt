599b4f3012af0a895f80696df4edf461fe7f12d5
Dima <d.d.demin@urfu.me>
2021-02-03 10:10:36 +0500

    OOP

--- StepikTasks/src/Main.kt
+++ StepikTasks/src/Main.kt
@@ -4,3 +4,18 @@ val scan = Scanner(System.`in`)
 fun main(){
     println(scan.nextInt())
 }
+
+open class Cat {
+    open fun hunt () {
+        println("Я охочусь на мышей, ведь я кот!")
+    }
+}
+
+class HomeCat: Cat(){
+    fun hunt(isSimpleCat: Boolean) {
+        if (isSimpleCat)
+            super.hunt()
+        else
+            println("Ты че сдурел? Какие мыши? Иди корми меня, ленивая задница!")
+    }
+}
